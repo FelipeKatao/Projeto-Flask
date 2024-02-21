590f20b4102c3d0f7eb6b22ddb5f5b14add34d52
FelipeKatao <cataofelipe@gmail.com>
2024-02-20 22:15:13 -0300

    update

--- app.py
+++ app.py
@@ -12,24 +12,23 @@ def helloWorld(contexto):
         return render_template("index.html",context=str(contexto),Carrinho = _Compras.CarrinhoUsuario())
     if(contexto == "vendas"):
         return render_template("index.html",context=str(contexto),Carrinho = _Compras.CarrinhoUsuario(),QuanidadeVendas =0)
+    if(contexto == "cadastro"):
+        return render_template("index.html",context=str(contexto),cadastro = True)
     return render_template("index.html",context=str(contexto))
   
-@app_site.post("/teste")
-def a():   
-    valor = 0 
-    return str(valor) 
-   
-@app_site.get("/dados/<dados>") 
-def ObterDados(dados): 
-    nome =request.args.get("nome")
-    if nome != None:
-        return {"Nome":nome}
-    return "Nome não cadastrado!"
+@app_site.post("/cad/produto")
+def CadProduto():
+    Status_erro = []
+    if(len(request.form["nome_produto"]) <=10 ):
+        Status_erro.append(" O Nome tem menos de 10 caracteres \n")
 
-@app_site.get("/funcao/somar/<numero>")
-def Somar(numero):
-    _calc = Calculo()
-    return {"ValorDaRemesa":str(_calc.Soma(int(request.args.get("v1")),int(request.args.get("v2"))))}
+    if(int(request.form["preco_produto"]) < 0):
+            Status_erro.append(" O preço é menor que 0 ")
+
+    if( Status_erro != ""):
+        return render_template("index.html",status=Status_erro,cadastro = True)
+    else:
+        return render_template("index.html",status="Produto cadastrado com sucesso",cadastro = True)
 
 if(__name__ == '__main__'):
     app_site.debug = True 
